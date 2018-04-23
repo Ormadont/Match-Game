38783b69519b81b6f66307e43517ccd161049485
Ormadont <evgeniy1demyanov@gmail.com>
2018-04-23 06:51:37 +0400

    simple commit

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -1,9 +1,13 @@
-var MatchGame = {};
+let MatchGame = {};
 
 /*
   Sets up a new game after HTML document has loaded.
   Renders a 4x4 board of cards.
 */
+
+$(document).ready(() =>{
+
+});
 
 /*
   Generates and returns an array of matching card values.
